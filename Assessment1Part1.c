ff7f13d72f8d17d22c405d862a61f2e7e0aa312b
Jess Haugh <jessica.haugh@uon.edu.au>
2019-04-02 02:49:58 +0000

    End of LabWk6:
    - tried to encrypt one variable
    
    - tried to start decrypting this variable

--- Assessment1Part1.c
+++ Assessment1Part1.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+  char in = 'a'; //Enter in letter
+  int out;       // change to 'char' when used in array, and then %d becomes %c
+                                 // int temp;   added in by demonstrator?
+                                 // temp = in;  added in by demonstrator?
+  int k = -24;   //Enter shift, -25<k<25, but if k=0 or 26, no shift occurs
+  
+  
+  /* printf ("Enter your letter\n");
+     scanf ("%c", &in);
+  
+     printf ("ASCII code for %c is %d\n", in, in);
+     printf ("Shift letter by: ");
+     scanf ("%d", &k); 
+     REMOVED 'SCANF' FROM CODE TO SAVE TIME GOING INTO TERMINAL */
+
+
+  out = (in+k)%26;
+    printf ("ASCII code for output is %d\n", out);
+    
+    
+  /*NEXT I NEED TO DECRYPT THIS CODE BACK TO A LETTER 
+    out = in + k;
+    out = (((in-26)+ k)%26)+97; */
+
+
+ /*char input[1000];   //{1,2,3...,100}
+   THIS IS THE NEXT STEP AFTER USING FORMULA TO STOP Z BEING SHIFTED 1 AND NOT RETURNING A*/
+  
  
- // ADD INSTRUCTIONS HERE
- return 0;
+ /* Formual to encrypt: e(x) = (m+k)(mod 26)
+               decrypt: d(c) = (c-k)(mod 26) w/ mod meaning % to get remainder */
+ 
+ 
+  return 0;
 }
